1aa462399ba60c4e3a17865ca42c942ec077750b
Raphael Chookagian <raphaelchookagian@gmail.com>
2022-08-31 16:06:23 -0700

    js fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
-console.log(hello-world);
+console.log('hello-world' );
 
-let usersName = prompt('What is your name?');
+let usersName = prompt('What is your name? ');
 let message;
 
 if (usersName == 'Kassie'){
@@ -9,16 +9,12 @@ if (usersName == 'Kassie'){
 } else if (usersName == 'Raphael') {
     message = "Heyo!";
 } else {
-    message = 'Welcome to my site!';
+    message = '  Welcome to my site!   ';
 }
-document.write('Hi' + usersName);
+
+
+document.write('Hi  ' + usersName +'!');
 document.write(message)
 
 
-
-document.write(Welcome to my page!);
-
-
-
-
-document.write(Do you like to hike or spend time in codethe outdoors?);
+document.write('   Do you like to hike or spend time in the outdoors?');
