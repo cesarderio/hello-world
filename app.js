414a4898eef4dcf3d6550a87716b6631f25561c7
Raphael Chookagian <raphaelchookagian@gmail.com>
2022-08-31 15:53:18 -0700

    JS

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 'use strict';
 console.log(hello-world);
 
+let usersName = prompt('What is your name?');
+let message;
+
+if (usersName == 'Kassie'){
+    message = 'Hello and welcome!'
+} else if (usersName == 'Raphael') {
+    message = "Heyo!";
+} else {
+    message = 'Welcome to my site!';
+}
+document.write('Hi' + usersName);
+document.write(message)
+
+
+
 document.write(Welcome to my page!);
+
+
+
+
+document.write(Do you like to hike or spend time in codethe outdoors?);
