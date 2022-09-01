02a1e853ff119ef246cee3d439f02c98a35d0e2f
Raphael Chookagian <raphaelchookagian@gmail.com>
2022-09-01 11:20:45 -0700

    adding

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 "use strict";
 console.log("hello-world");
 
+let welcome = prompt('Do you like to hike?');
+let message;
+
+if (welcome == "yes") {
+    message = "We can be friends!";
+} else ( welcome == "no") {
+    message = "Why not?";
+}
+
 let usersName = prompt("What is your name? ");
 let message;
 
