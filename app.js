f75e7ff63145e9757416741c6089a9dc82762846
Raphael Chookagian <raphaelchookagian@gmail.com>
2022-08-31 17:03:15 -0700

    js update

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-'use strict';
-console.log('hello-world' );
+"use strict";
+console.log("hello-world");
 
-let usersName = prompt('What is your name? ');
+let usersName = prompt("What is your name? ");
 let message;
 
-if (usersName == 'Kassie'){
-    message = 'Hello and welcome!'
-} else if (usersName == 'Raphael') {
-    message = "Heyo!";
+if (usersName == "Kassie") {
+  message = "Hello and welcome!";
+} else if (usersName == "Raphael") {
+  message = "Heyo!";
 } else {
-    message = '  Welcome to my site!   ';
+  message = "  Welcome to my site!   ";
 }
 
+document.write("Hi  " + usersName + "!");
+document.write(message);
 
-document.write('Hi  ' + usersName +'!');
-document.write(message)
-
-
-document.write('   Do you like to hike or spend time in the outdoors?');
+document.write("   Do you like to hike or spend time in the outdoors?");
